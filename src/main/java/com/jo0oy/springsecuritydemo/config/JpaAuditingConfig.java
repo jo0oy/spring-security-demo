ae296c5d331f4c98335517b843b3e242bbc52397
jo0oy <rkfkrh07@naver.com>
2023-11-08 03:19:16 +0900

    Feat: Jpa Auditing
    - jpa auditing 설정 추가

---
+++ src/main/java/com/jo0oy/springsecuritydemo/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.jo0oy.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
