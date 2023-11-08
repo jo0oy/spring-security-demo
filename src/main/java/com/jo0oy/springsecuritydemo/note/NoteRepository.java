f777fccd09e86e41e928c1148ebfb358e898c7da
jo0oy <rkfkrh07@naver.com>
2023-11-09 03:02:35 +0900

    Refactor: JPQL 에서 Custom하게 Sorting이 안됨 --> 정적 정렬으로 변경
    - before
       - JPQL 문에 @Param 으로 정렬 기준 Property 받아서 동적으로 처리하고 함
       - But, 예상한대로 동작하지 않음. 정렬 적용 X
    - after
       - Note id 기준 내림차순 정렬로 정적 정렬하도록 변경
       - 메서드명 변경 : findAllWithUserOrderByIdDesc()
    - 메서드 변경에 따라서 해당 메서드 사용하는 곳 연쇄 수정 업데이트

--- src/main/java/com/jo0oy/springsecuritydemo/note/NoteRepository.java
+++ src/main/java/com/jo0oy/springsecuritydemo/note/NoteRepository.java
@@ -3,15 +3,14 @@ package com.jo0oy.springsecuritydemo.note;
 import com.jo0oy.springsecuritydemo.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
-    @Query("select n from Note n join fetch n.user order by :property desc")
-    List<Note> findAllWithUserOrderByDesc(@Param("property") String property);
+    @Query("select n from Note n join fetch n.user order by n.id desc")
+    List<Note> findAllWithUserOrderByIdDesc();
 
     List<Note> findAllByUserOrderByIdDesc(User user);
 
